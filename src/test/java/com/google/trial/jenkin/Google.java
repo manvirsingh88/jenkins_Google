2de107201de8f9027e3a022f247fc48257ce492b
Manvir <manvir.singh.parmar@gmail.com>
2020-11-23 22:52:16 -0500

    Boni Garcia Added

--- src/test/java/com/google/trial/jenkin/Google.java
+++ src/test/java/com/google/trial/jenkin/Google.java
@@ -9,12 +9,16 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class Google {
 
 	WebDriver driver;
 
 	@BeforeMethod
 	public void setup() {
+		
+		WebDriverManager.chromedriver().setup();
 
 		driver = new ChromeDriver();
 
